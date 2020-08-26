99003352f58c2d7fbde32f545102e723e7391853
tokin89 <tokin8989@gmail.com>
2020-08-26 21:46:16 +0300

    commit1

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 "use strict";
-const answers = [];
-answers[0] = prompt("как тебя зову?", "");
-answers[1] = prompt("сколько тебе лет?", "");
-answers[2] = prompt("откуда ты?", "");
-console.log(typeof(answers));
+let numberOfFilms = [];
+numberOfFilms = +prompt('сколько фильмов ты посмотрел');
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    let: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('один из последних просмотренных фильмов?'),
+b = prompt('на сколько его оцените?'),
+c = prompt('один из последних просмотренных фильмов?'),
+d = prompt('на сколько его оцените?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
+
+// const pol = 'pashtet';
+// console.log(`${pol} ${"придурок"}`);
+// console.log(pol);
+// const answers = [];
+// answers[0] = prompt("как тебя зову?", "");
+// answers[1] = prompt("сколько тебе лет?", "");
+// answers[2] = prompt("откуда ты?", "");
+// console.log(typeof(answers));
 // console.log(answers[0]);
 // console.log(answers[1]);
 // console.log(answers[2]);
