b9bdc4b707da656e6392193d5faea08d9fa21e2f
tokin89 <tokin8989@gmail.com>
2020-08-26 20:44:42 +0300

    first

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+const answers = [];
+answers[0] = prompt("как тебя зову?", "");
+answers[1] = prompt("сколько тебе лет?", "");
+answers[2] = prompt("откуда ты?", "");
+console.log(typeof(answers));
+// console.log(answers[0]);
+// console.log(answers[1]);
+// console.log(answers[2]);
+// const answer = prompt("вам есть 18?","да");
+// console.log(answer);
+// const result = confirm("Are you here?");
+// console.log(result);
+// let number = 5;
+// const leftBorderWidth = 1;
+// console.log(number);
+// const person = "pol";
+// console.log(person);
+// const obj ={
+//     name: "jone",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj.name);
